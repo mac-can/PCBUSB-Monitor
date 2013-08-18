6a7884068cc2ac6d3ea1f4b3b0dbec225d4f2896
neptun <neptun@032d5e43-dad4-674c-ae8e-2180808bc3a5>
2013-08-18 13:27:11 +0000

    [MACCAN-3] Initial import
    git-svn-id: https://svn.uv-software.net/sourcemedley/projects/mac/programs/CAN/MacCAN%20Monitor@207 032d5e43-dad4-674c-ae8e-2180808bc3a5

---
+++ trunk/MacCAN Monitor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MacCAN Monitor
+//
+//  Created by Uwe Vogt on 18.08.13.
+//  Copyright (c) 2013 UV Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ trunk/MacCAN Monitor/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  MacCAN Monitor
+//
+//  Created by Uwe Vogt on 18.08.13.
+//  Copyright (c) 2013 UV Software. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
